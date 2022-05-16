037ea4c05b6972bac0b901a7cf94c76dc4ad8765
Паша Луковников <lukovnikov.pasha@gmail.com>
2022-05-16 18:25:06 +1000

    ++

---
+++ MyBot/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBot
+{
+    public class Helper
+    {
+        //Scaffold-DbContext "Server=DESKTOP-2KIP198\SQLEXPRESS;Initial Catlog=MyBot_DB;Trusted_Connection=True; User=dbo" Microsoft.EntityFrameworkCore.SqlServer -OutputDir db
+    }
+}
